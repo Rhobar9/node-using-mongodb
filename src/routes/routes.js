c7eb01dde8b1738713f84d2f948b591f352858de
Rhobar9 <86721432+Rhobar9@users.noreply.github.com>
2022-09-30 17:37:32 +0200

    Module3_attempt

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,16 @@
-import { addnewProduct } from '../controllers/controllers';
+import { addnewProduct, getProductWithID, getProducts } from '../controllers/controllers';
 
 const routes = (app) => {
     app.route('/products')
+        //get multiple products
+        .get(getProducts)
 
         //Post endpoint
         .post(addnewProduct);
+
+    app.route('/products/:ProductID')
+        //get a specific product with an ID
+        .get(getProductWithID);
 }
 
 export default routes;
